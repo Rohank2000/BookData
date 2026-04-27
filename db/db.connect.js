06d688f0f4c8e621ad4f8b3fcdd914347b30882e
Rohan <rk2024@duck.com>
2026-04-27 17:19:41 +0530

    chore: Updated Database Connection Part of db.connect file.

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,8 +4,12 @@ require("dotenv").config();
 
 const mongoMetaData = process.env.MONGODB_URI;
 
-const UserDataToMongo = async ()=>{
-await mongoose.connect(mongoMetaData).then(()=>{console.log("Connected to Database.");}).catch((error)=>console.log("Error Connecting to Database.", error));
-};
+let cachedDb = null;
 
+const UserDataToMongo = async()=> {
+  if (cachedDb) return cachedDb;
+await mongoose.connect(mongoMetaData).then(()=>{console.log("Connected to Database.");}).catch((error)=>console.log("Error Connecting to Database.", error));
+
+  return cachedDb;
+}
 module.exports = {UserDataToMongo};
